a7fd31d38bd081b6264e9d223bae9b5e0841d785
Lucas Oliveira <lucas.oliveira@basis.com.br>
2018-12-04 23:49:47 -0200

    create login page and categories page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage, MenuController } from 'ionic-angular';
 
+@IonicPage() //it says this can refereced like page and like string
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public menu: MenuController
+  ) {
 
   }
 
+  ionViewWillEnter() {
+    this.menu.swipeEnable(false);
+  }
+
+  ionViewWillLeave() {
+    this.menu.swipeEnable(true);
+  }
+
+  login() {
+    this.navCtrl.setRoot('CategoriesPage');
+  }
 }
